5db08c9e2112a38337da661b76bae3a4b250d689
Nandeep <n9986.mali@gmail.com>
2016-05-02 11:23:33 +0530

    Initial commit.

---
+++ build/CMakeFiles/imgCapture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgCapture.dir/src/face_train.cpp.o"
+  "imgCapture.pdb"
+  "imgCapture"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imgCapture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
